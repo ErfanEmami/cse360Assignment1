b21056467e34a5efbf23f9c0f980916c9d3d6fcb
Erfan <erfan.emami123@gmail.com>
2020-10-05 22:17:41 -0700

    initial commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+/*
+ * Name: Erfan Emami
+ * ASU ID: 121 525 2727
+ * Assignment: Week 7 homework includes creating cse360assignment02, creating a local repository, and committing it to GitHub. This is to practice
+ * version control
+ */
+
+package cse360assignment02;
+
+public class AddingMachine
+{
+    
+    private int total;
+
+    public AddingMachine ()
+    {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * total added/subtracted
+     */
+    public int getTotal ()
+    {
+        return 0;
+    }
+
+    /**
+     * @param value value gets added to total
+     */
+    public void add (int value)
+    {
+    }
+
+    /**
+     * @param value value gets subtracted from total
+     */
+    public void subtract (int value)
+    {
+    }
+
+    /**
+     * clear transaction history and total
+     */
+    public void clear()
+    {
+    }
+
+    /**
+     * return transaction history
+     */
+    public String toString ()
+    {
+        return "";
+    }
+
+}
